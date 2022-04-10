fe1c4551d05c04e659a52811685d880a43082eda
Amit Hakim <amitoka10@gmail.com>
2022-04-10 22:52:40 +0300

    main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import logo from './icons/logo.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            moveToVideoPage: false
+        }
+    }
+
+    onStartClick = () => {
+        this.setState({
+            ...this.state,
+            moveToVideoPage: true
+        })
+    };
+
+    render() {
+        const {moveToVideoPage} = this.state;
+
+        return (
+            <div className="main-container">
+                <div className="main-page-titles">
+                <div className="main-title">Welcome to SLTT</div>
+                <div className="second-title">You do the sign, we do the text</div>
+                </div>
+                <img src={logo} alt="logo" className={moveToVideoPage ? "logo-center" : "logo-center"}/>
+                <button className="press-btn" onClick={this.onStartClick}>Press Start</button>
+            </div>
+        );
+    }
+
 }
 
 export default App;
